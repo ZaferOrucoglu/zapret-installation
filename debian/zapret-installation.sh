afa1b1e3f767685523914ed7d143dc550d1cecb2
ZaferOrucoglu <172007734+ZaferOrucoglu@users.noreply.github.com>
2026-06-16 21:45:08 +0300

    debian setcap issue fix

--- debian/zapret-installation.sh
+++ debian/zapret-installation.sh
@@ -84,14 +84,13 @@ done
 sudo systemctl enable --now dnscrypt-proxy
 
 # Setting up dnscrypt-proxy as a DNS resolver
-sudo chattr -i /etc/resolv.conf 2>/dev/null || true
 sudo install -d -m 755 /etc/NetworkManager/conf.d
 printf "[main]\ndns=none\n" | sudo tee /etc/NetworkManager/conf.d/dns.conf >/dev/null
 echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf >/dev/null
-sudo chattr +i /etc/resolv.conf # protects resolv.conf against NetworkManager
 
 sudo systemctl restart dnscrypt-proxy
 sudo systemctl restart NetworkManager
+sudo setcap cap_net_bind_service=+ep /usr/sbin/dnscrypt-proxy
 
 echo "the file /etc/resolv.conf will be protected against NetworkManager!"
 echo "if you want to make /etc/resolv.conf writable again, run:"
